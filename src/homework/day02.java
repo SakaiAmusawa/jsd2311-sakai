5b64a33a5c9a8a07e633180a45ff46b234f8b01b
sakai <sakai263016@gmail.com>
2023-11-30 19:37:05 +0800

    sakai

--- src/homework/day02.java
+++ src/homework/day02.java
@@ -1,5 +1,7 @@
 package homework;
 
+import static sakai.DividingLine.dividingLine;
+
 public class day02 {
     public static void main(String[] args) {
      /*   变量的练习：
@@ -38,6 +40,7 @@ public class day02 {
         ......
         int class 关键词禁止使用
          */
+        dividingLine();
     /*
     基本数据类型的练习：int
 
@@ -53,7 +56,7 @@ public class day02 {
         int a3 = 2000000000;
         int a4 = 300000000;
         System.out.println(a3 + a4);//溢出
-
+        dividingLine();
         /*
         基本数据类型的练习：long
 
@@ -61,9 +64,94 @@ public class day02 {
    - 声明变量存储几个较大数据的运算，演示：若有可能溢出建议将L设计在第1个数字后。
 
          */
-        long  b1 = 2000000000L;
+        long b1 = 2000000000L;
         long b2 = 30000000000L;
+        long b3 = 1000000000000L * 2000 * 1000;
+        System.out.println(b3);
+        dividingLine();
 
+        /*
+        基本数据类型的练习：double
+
+   - 声明初始化两个以上的浮点型变量
+   - 声明几个浮点型变量并将它们做数学操作，演示double运算的舍入误差问题
+         */
+        double c1 = 3.16;
+        double c2 = 2.14;
+        double c3 = 4.6;
+        System.out.println(c1 - c2);
+        System.out.println(c1 + c2);
+        System.out.println(c1 + c3);
+        System.out.println(c1 - c3);
+        dividingLine();
+
+        /*
+        5. 基本数据类型的练习：boolean
+
+   - 声明初始化两个以上的布尔型变量
+         */
+        boolean d1 = true;
+        boolean d2 = false;
+
+        /*
+        6. 基本数据类型的练习：char
+
+   - 声明初始化五个以上的字符型变量
+   - 声明字符型变量，演示转义符
+         */
+
+        char char1 = '男';
+        char char2 = 'm';
+        char char3 = '3';
+        char char4 = 'A';
+        char char5 = ' ';
+
+        char char6 = '\'';
+        System.out.println(char6);
+        char char7 = '\\';
+        System.out.println(char7);//通过转义符来达到输出特殊符号的目的
+        dividingLine();
+
+        /*
+        基本数据类型的转换：两种方式
+
+   - 声明几个变量，演示自动类型转换和强制类型转换的语法
+   - 声明一个较大类型的长整型变量，演示强转可能会发生的溢出问题
+   - 声明一个较大精度的浮点型变量，演示强转可能会发生的丢失精度问题
+         */
+        int e1 = 5;
+        long e2 = e1;
+        int e3 = (int) e2;
+
+        int e4 = (int) 5.2;
+        System.out.println(e4);
+
+        long e5 = 100000000000L;
+        int e6 = (int) e5;
+        System.out.println(e6);
+
+        double e7 = 3.16;
+        int e8 = (int) e7;
+        System.out.println(e8);
+        dividingLine();
+        /*
+        8. 基本数据类型的转换：两点规则
+
+   - 声明两个byte型变量b1和b2，并赋值为直接量
+   - 声明一个byte型变量，并赋值为b1与b2的和
+
+         */
+        byte f1 = 1;
+        byte f2 = 5;
+        byte f3 = (byte) (f1 + f2);
+        System.out.println(f3);
+
+        byte f4 = '1';
+        byte f5 = 'A';
+        System.out.println(f4);
+        System.out.println(f5);
+        byte f6 = (byte) (f4 + f5);
+        System.out.println(f6);
 
     }
 }
---
+++ src/sakai/DividingLine.java
@@ -0,0 +1,8 @@
+package sakai;
+
+public class DividingLine {
+    public static void dividingLine() {
+        String dividingLine = "------------";
+        System.out.println(dividingLine);
+    }
+}
