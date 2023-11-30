5d80f56485470df9e77b66600e40ce49d715bd58
sakai <sakai263016@gmail.com>
2023-11-30 09:43:28 +0800

    变量的声明与初始化

---
+++ src/day02/VarDemo.java
@@ -0,0 +1,16 @@
+package day02;
+
+//变量演示
+public class VarDemo {
+    public static void main(String[] args) {
+        // 1)声明一个变量
+        int a;
+        int b, c, d;
+
+        //2)变量的初始化
+        int e = 10;
+        int f;
+        f = 20;
+        int g = 30, h = 40;
+    }
+}
