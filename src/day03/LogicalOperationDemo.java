44d7c7a4fcac36aedee550889ea93b86c93bedc6
sakai <sakai263016@gmail.com>
2023-12-01 11:42:49 +0800

    sakai

--- src/day03/LogicalOperationDemo.java
+++ src/day03/LogicalOperationDemo.java
@@ -1,11 +1,12 @@
 package day03;
-// 逻辑与运算符 && 示例
+// 逻辑与运算符 && 、 || 、 ！ 示例
 
 import static sakai.DividingLine.dividingLine;
 
 public class LogicalOperationDemo {
     public static void main(String[] args) {
         int a = 5, b = 10, c = 5;
+        // 逻辑与运算符 &&
         boolean b1 = a >= c && c <= b;//true&&true = true
         System.out.println(b1);
 
@@ -23,6 +24,21 @@ public class LogicalOperationDemo {
         //判断成绩是否在60到100之间
         int score = 70;
         System.out.println(score >= 60 && score <= 100);
+        dividingLine();
 
+        // 逻辑与运算符 ||
+        System.out.println(a == b || a == c);//false||true =true
+        System.out.println(a > b || b > c);//false||true = true
+        System.out.println(a != b || a + c > b);//true||true = true
+        System.out.println(b == a || b - a != c);//false||false = false
+        dividingLine();
+
+        //||的实际应用,判断成绩是否合法（在0~100之内）
+        System.out.println(score < 0 || score > 100);//false不在该范围内 合法，true在该范围内 不合法
+        dividingLine();
+
+        //逻辑运算符 !(!的优先级高)
+        System.out.println(!(a > b));//a>b:false !a>b :true
+        System.out.println(!(a == c));//a==c:true !a==c:false
     }
 }
