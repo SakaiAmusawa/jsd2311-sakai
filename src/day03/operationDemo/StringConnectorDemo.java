ec016c1f45abcdff56f9ee51156c6aaa1f1a1c0e
sakai <sakai263016@gmail.com>
2023-12-01 14:35:40 +0800

    sakai

--- src/day03/operationDemo/StringConnectorDemo.java
+++ src/day03/operationDemo/StringConnectorDemo.java
@@ -2,6 +2,9 @@ package day03.operationDemo;
 
 public class StringConnectorDemo {
     public static void main(String[] args) {
-
+        String name = "zjw";
+        int age = 24;
+        System.out.println("大家好，我叫" + name + "，今年" + age + "岁了.");
+        System.out.println(name + "我今年" + age + "岁");
     }
 }
