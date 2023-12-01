9c80f46314e48109e02c089afab3bf486faff54b
sakai <sakai263016@gmail.com>
2023-12-01 10:35:05 +0800

    sakai

--- src/day03/RelationalOperatorDemo.java
+++ src/day03/RelationalOperatorDemo.java
@@ -10,8 +10,12 @@ public class RelationalOperatorDemo {
         System.out.println(a % 2 > 1);//false a%2的值为1
         System.out.println(a != c);//false
         System.out.println(a + c > b);//false
+        System.out.println((b % a) > c);//false
         System.out.println(a++ > 5);//false
         System.out.println(a);//a的值为6
+        System.out.println(a++ > 5);//true
+        System.out.println(a);
+
 
     }
 }
