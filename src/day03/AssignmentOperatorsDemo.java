17e84dc5123f59dd5c6a057795659dad544dd8a8
sakai <sakai263016@gmail.com>
2023-12-01 14:21:09 +0800

    sakai

---
+++ src/day03/AssignmentOperatorsDemo.java
@@ -0,0 +1,25 @@
+package day03;
+
+public class AssignmentOperatorsDemo {
+    public static void main(String[] args) {
+        //辅助运算符自带强转功能
+        int a = 5;
+        a += 10;//a=a+10 a=(int)(a+10)
+        System.out.println(a);
+
+        a *= 2;//a=a*2 a=(int)(a*2)
+        System.out.println(a);
+        a /= 6;//a=a/6 a=(int)(a/6)
+        System.out.println(a);
+
+/*        //小面试题
+        short s = 5;
+        s=s+10;
+        s+=10;
+        解析：
+        short s = 5; 5时int类型的数据可以直接赋值给short类型
+        s = s + 10; 报错 s是short类型 但10是int类型 所以结果会转为int类型 但是结果要赋值给 s 它是short类型 所以需要强转 为：s = (short)(s+10)
+        s+=10;正确 是应为 赋值运算符自带强转效果
+        */
+    }
+}
