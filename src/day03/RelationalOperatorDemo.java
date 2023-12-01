e3541ec9c8c615614f143c4d75c4927e87d71e05
sakai <sakai263016@gmail.com>
2023-12-01 10:32:28 +0800

    sakai

--- src/day03/RelationalOperatorDemo.java
+++ src/day03/RelationalOperatorDemo.java
@@ -2,6 +2,16 @@ package day03;
 
 public class RelationalOperatorDemo {
     public static void main(String[] args) {
+        int a = 5, b = 10, c = 5;
+        boolean b1 = a == c;
+        System.out.println(b1);
+
+        System.out.println(a + c == b);//true
+        System.out.println(a % 2 > 1);//false a%2的值为1
+        System.out.println(a != c);//false
+        System.out.println(a + c > b);//false
+        System.out.println(a++ > 5);//false
+        System.out.println(a);//a的值为6
 
     }
 }
