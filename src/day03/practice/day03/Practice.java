40f2f7351d6631db60a298b34d7653f83b0a6669
sakai <sakai263016@gmail.com>
2023-12-02 11:43:45 +0800

    practice

--- src/day03/practice/day03/Practice.java
+++ src/day03/practice/day03/Practice.java
@@ -11,5 +11,24 @@ public class Practice {
         boolean result = ((num1++ > 7) && (num2-- > 5));
         System.out.println(result + ", " + num1 + ", " + num2);
         //true 9 5
+
+        int num = 15;
+        boolean check = num > 10 && num % 2 == 0 || num % 3 == 0;//true false || true
+        System.out.println(check);//true
+
+        int number = 45;
+        String result1 = "";
+
+        if (number % 5 == 0) {
+            result1 += "可被 5 整除 ";
+        } else if (number % 9 == 0) {
+            result1 += "可被 9 整除 ";
+        } else {
+            result1 += "不能被 5 或 9 整除 ";
+        }
+
+        System.out.println(result1);
+
+
     }
 }
