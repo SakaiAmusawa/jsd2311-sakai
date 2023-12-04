a880772eabf285614325168b9786aff37417da38
sakai <sakai263016@gmail.com>
2023-12-04 17:14:09 +0800

    sakai

---
+++ src/day04/practice/AccumulationByFor.java
@@ -0,0 +1,12 @@
+package day04.practice;
+
+//1到100的累加和（结果：5050）
+public class AccumulationByFor {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 1; i <= 100; i++) {
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+}
