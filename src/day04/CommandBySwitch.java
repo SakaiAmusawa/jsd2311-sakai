1b7973b215cfe8ba016854ff74048c85d66cfa38
sakai <sakai263016@gmail.com>
2023-12-04 10:28:10 +0800

    命令解析程序

--- src/day04/CommandBySwitch.java
+++ src/day04/CommandBySwitch.java
@@ -1,12 +1,28 @@
 package day04;
+//命令解析工具
 
 import java.util.Scanner;
 
 public class CommandBySwitch {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("please enter command:");
+        System.out.print("please select function: 1.存款 2.取款 3.余额查询 4.退卡");
         int command = scanner.nextInt();
-        System.out.println("command:" + command);
+        switch (command) {
+            case 1:
+                System.out.println("存款");
+                break;
+            case 2:
+                System.out.println("取款");
+                break;
+            case 3:
+                System.out.println("余额查询");
+                break;
+            case 4:
+                System.out.println("退卡成功！");
+                break;
+            default:
+                System.out.println("error!");
+        }
     }
 }
